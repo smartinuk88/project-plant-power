c1a653021708db244ff3de68abd04485e1bc6213
Scott Martin <scott.martin88@live.com>
2022-02-22 08:20:03 +0100

    API call to fake store api to pull in temporary products, and SSR complete

---
+++ components/ProductFeed.js
@@ -0,0 +1,11 @@
+function ProductFeed({ products }) {
+  return (
+    <div>
+      {products.map((product) => (
+        <p>{product.title}</p>
+      ))}
+    </div>
+  );
+}
+
+export default ProductFeed;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import ProductFeed from "../components/ProductFeed";
 import Banner from "../components/Banner";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className>
       <Head>
@@ -24,8 +23,21 @@ export default function Home() {
 
         <main className="max-w-6xl">
           <Banner />
+          <ProductFeed products={products} />
         </main>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
